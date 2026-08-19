d3fd67c72e38ab736afa5d9777775a2c1c1d3fe0
SilverWolfiee <rblenderdrag@gmail.com>
2026-08-19 11:53:17 +0700

    fix(database) : fix pathing issue

--- src/database.py
+++ src/database.py
@@ -1,15 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./app.db"
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "app.db")
+
+DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread":False}
+    DATABASE_URL, 
+    connect_args={"check_same_thread": False}
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
